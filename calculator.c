5e2c67cac0b683608366c1ec9a1dc18803880d6b
dani <rosario.dani01@gmail.com>
2026-01-09 09:42:08 -0300

    first commit

---
+++ calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float num1, num2, result;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("Enter an operator (+, -, *, /, ^): ");
+    scanf(" %c", &operator);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    
+
+    return 0;
+}
