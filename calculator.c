84c2628b2cfa76eef11bbfd186d5a9ba55f4cb1e
dani <rosario.dani01@gmail.com>
2026-01-09 09:57:49 -0300

    add simple operations

--- calculator.c
+++ calculator.c
@@ -5,6 +5,8 @@ int main()
     float num1, num2, result;
     char operator;
 
+    printf("Simple Calculator in C\n");
+
     printf("Enter first number: ");
     scanf("%f", &num1);
     printf("Enter an operator (+, -, *, /, ^): ");
@@ -12,7 +14,43 @@ int main()
     printf("Enter second number: ");
     scanf("%f", &num2);
 
+    switch(operator)
+    {
+        case '+':
+            result = num1 + num2;
+            printf("%.2f + %.2f = %.2f\n", num1, num2, result);
+            break;
+        
+        case '-':
+        result = num1 - num2;
+            printf("%.2f - %.2f = %.2f\n", num1, num2, result);
+            break;
+        
+        case '*':
+        result = num1 * num2;
+            printf("%.2f * %.2f = %.2f\n", num1, num2, result);
+            break;
+        
+        case '/':
+            if(num2 != 0)
+            {
+                result = num1 / num2;
+                printf("%.2f / %.2f = %.2f\n", num1, num2, result);
+            }
+            else
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        
+        case '^':
+            result = pow(num1, num2);
+            printf("%.2f ^ %.2f = %.2f\n", num1, num2, result);
+            break;
+        
+        default:
+            printf("Error: Invalid operator.\n");
+    }
     
-
     return 0;
 }
